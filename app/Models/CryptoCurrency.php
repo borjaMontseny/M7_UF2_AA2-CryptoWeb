60595cc942ae1f1574c903da0cf714ffe9a184e7
ericsaza-InsBc <eric.salado@insbaixcamp.cat>
2023-11-20 20:17:33 +0100

    Model CryptoCurrency configurat.

--- app/Models/CryptoCurrency.php
+++ app/Models/CryptoCurrency.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class CryptoCurrency extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'cryptocurrencies';
+
+    protected $fillable = [
+        'rank',
+        'name',
+        'symbol',
+        'slug',
+        'price',
+        'volume_1h',
+        'volume_24h',
+        'volume_7d',
+        'market_cap',
+        'volume_24h',
+        'total_supply',
+    ];
 }
