f8c41a316ffe05d35672d19d8059c5a20aaf6fff
AdriaChillon <144825302+AdriaChillon@users.noreply.github.com>
2023-11-20 21:28:56 +0100

    Seeders, llamada a la api insertando en BD

--- config/services.php
+++ config/services.php
@@ -30,5 +30,10 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    
+    'coinmarketcap' => [
+        'base_uri' => 'https://pro-api.coinmarketcap.com/v1/',
+        'api_key' => env('API_KEY'),
+    ],
 
 ];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,19 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
-class DatabaseSeeder extends Seeder
+class DataBaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this -> call(CryptoCurrencySeeder::class);
     }
 }
